2ad31f2da6b885b5e7307f70adecccab64bb65fa
Sam Harwell <sam@tunnelvisionlabs.com>
2015-01-12 20:28:51 -0600

    Add atn_deserialization_options

---
+++ antlr4cpp/antlr/v4/runtime/atn/atn_deserialization_options.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+namespace antlr4 {
+namespace runtime {
+namespace atn {
+
+	class atn_deserialization_options
+	{
+	private:
+		bool _verify_atn;
+		bool _generate_rule_bypass_transitions;
+		bool _optimize;
+
+	public:
+		atn_deserialization_options()
+			: _verify_atn(true)
+			, _generate_rule_bypass_transitions(false)
+			, _optimize(true)
+		{
+		}
+
+	public:
+		static atn_deserialization_options default_options()
+		{
+			return atn_deserialization_options();
+		}
+
+	public:
+		bool verify_atn() const
+		{
+			return _verify_atn;
+		}
+
+		void verify_atn(bool value)
+		{
+			_verify_atn = value;
+		}
+
+		bool generate_rule_bypass_transitions() const
+		{
+			return _generate_rule_bypass_transitions;
+		}
+
+		void generate_rule_bypass_transitions(bool value)
+		{
+			_generate_rule_bypass_transitions = value;
+		}
+
+		bool optimize() const
+		{
+			return _optimize;
+		}
+
+		void optimize(bool value)
+		{
+			_optimize = value;
+		}
+	};
+
+}
+}
+}
