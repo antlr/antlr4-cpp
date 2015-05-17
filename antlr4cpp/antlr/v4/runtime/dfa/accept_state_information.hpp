9424a5d62c77508226b0a4857f90e3d7f70f8864
Sam Harwell <sam@tunnelvisionlabs.com>
2015-05-16 21:06:09 -0500

    Implement accept_state_information

---
+++ antlr4cpp/antlr/v4/runtime/dfa/accept_state_information.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <memory>
+
+namespace antlr4 {
+namespace runtime {
+
+	namespace atn {
+		class lexer_action_executor;
+	}
+
+namespace dfa {
+
+	class accept_state_information
+	{
+	private:
+		size_t _prediction;
+		std::shared_ptr<atn::lexer_action_executor> _lexer_action_executor;
+
+	public:
+		accept_state_information(size_t prediction)
+			: _prediction(prediction)
+			, _lexer_action_executor(nullptr)
+		{
+		}
+
+		accept_state_information(size_t prediction, std::shared_ptr<atn::lexer_action_executor> const& lexer_action_executor)
+			: _prediction(prediction)
+			, _lexer_action_executor(lexer_action_executor)
+		{
+		}
+
+	public:
+		size_t prediction() const
+		{
+			return _prediction;
+		}
+
+		std::shared_ptr<atn::lexer_action_executor> const& lexer_action_executor() const
+		{
+			return _lexer_action_executor;
+		}
+	};
+
+}
+}
+}
